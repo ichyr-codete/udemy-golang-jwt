3e62995dcd5ee6be4cc4b4aa71e34b0cf2f50a49
Ivan Chyr - Codete <ivan.chyr@codete.com>
2020-01-01 20:16:57 +0100

    finished creating signup function

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/mux"
 	"github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -25,9 +26,10 @@ type Error struct {
 	Message string `json:"message"`
 }
 
+var db *sql.DB
+
 func main() {
 	router := mux.NewRouter()
-	var db *sql.DB
 
 	pgUrl, err := pq.ParseURL("postgres://amuodazc:jsXKFS4Fe4gNvlmOQcYVG6SAHFejmp99@balarama.db.elephantsql.com:5432/amuodazc")
 	if err != nil {
@@ -38,7 +40,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	db.Close()
 
 	router.HandleFunc("/signup", signup).Methods("POST")
 	router.HandleFunc("/login", login).Methods("POST")
@@ -53,6 +54,10 @@ func main() {
 func respondWithError(res http.ResponseWriter, status int, error Error) {
 	res.WriteHeader(status)
 	json.NewEncoder(res).Encode(error)
+}
+
+func respondWithJSON(res http.ResponseWriter, data interface{}) {
+	json.NewEncoder(res).Encode(data)
 }
 
 func signup(res http.ResponseWriter, req *http.Request) {
@@ -72,6 +77,28 @@ func signup(res http.ResponseWriter, req *http.Request) {
 		respondWithError(res, http.StatusBadRequest, error)
 		return
 	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	user.Password = string(hash)
+
+	insertStatement := "insert into users (email, password) values($1, $2) returning id;"
+	err = db.QueryRow(insertStatement, user.Email, user.Password).Scan(&user.ID)
+	if err != nil {
+		error.Message = err.Error()
+		respondWithError(res, http.StatusInternalServerError, error)
+		return
+	}
+
+	// as we want to return user object in response, we want to hide password
+	// todo ask why in Q&A
+	user.Password = ""
+
+	res.Header().Set("Content-Type", "application/json")
+	respondWithJSON(res, user)
 }
 
 func login(res http.ResponseWriter, req *http.Request) {
